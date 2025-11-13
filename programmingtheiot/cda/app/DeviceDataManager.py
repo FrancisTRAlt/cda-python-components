58fe899fc8dee3fa66b0c9fccabbabf84a957d83
Francis Tran <xst-tran6832@stthomas.edu>
2025-11-13 11:24:58 -0600

    PIOT-CDA-10-004 Semi

--- programmingtheiot/cda/app/DeviceDataManager.py
+++ programmingtheiot/cda/app/DeviceDataManager.py
@@ -121,16 +121,24 @@ class DeviceDataManager(IDataMessageListener):
 		"""
 		pass
 	
-	def handleSensorMessage(self, data: SensorData) -> bool:
-		"""
-		This callback method will be invoked by the sensor manager that just processed
-		a new sensor reading, which creates a new SensorData instance that will be
-		passed to this method.
-		
-		@param data The incoming SensorData message.
-		@return boolean
-		"""
-		pass
+	def handleSensorMessage(self, data: SensorData = None) -> bool:
+		if data:
+			logging.info("Incoming sensor data received (from sensor manager): " + str(data))
+			
+			# TODO: Optionally, implement `_handleSensorDataAnalysis()` to handle internal analytics
+			self._handleSensorDataAnalysis(data = data)
+			
+			# Convert the `SensorData` instance to JSON
+			jsonData = DataUtil().sensorDataToJson(data = data)
+			
+			# Pass the resource and newly generated JSON data to `_handleUpstreamTransmission()`
+			self._handleUpstreamTransmission(resource = ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, msg = jsonData)
+			
+			return True
+		else:
+			logging.warning("Incoming sensor data is invalid (null). Ignoring.")
+			
+			return False
 	
 	def handleSystemPerformanceMessage(self, data: SystemPerformanceData) -> bool:
 		"""
@@ -182,11 +190,19 @@ class DeviceDataManager(IDataMessageListener):
 		"""
 		pass
 		
-	def _handleUpstreamTransmission(self, resourceName: ResourceNameEnum, msg: str):
-		"""
-		Call this from handleActuatorCommandResponse(), handlesensorMessage(), and handleSystemPerformanceMessage()
-		to determine if the message should be sent upstream. Steps to take:
-		1) Check connection: Is there a client connection configured (and valid) to a remote MQTT or CoAP server?
-		2) Act on msg: If # 1 is true, send message upstream using one (or both) client connections.
-		"""
-		pass
+	def _handleUpstreamTransmission(self, resource = None, msg: str = None):
+		logging.info("Upstream transmission invoked. Checking comm's integration.")
+		
+		# NOTE: If using MQTT, the following will attempt to publish the message to the broker
+		if self.mqttClient:
+			if self.mqttClient.publishMessage(resource = resource, msg = msg):
+				logging.debug("Published incoming data to resource (MQTT): %s", str(resource))
+			else:
+				logging.warning("Failed to publish incoming data to resource (MQTT): %s", str(resource))
+		
+		# NOTE: If using CoAP, the following will attempt to PUT the message to the server
+		if self.coapClient:
+			if self.coapClient.sendPutRequest(resource = resource, payload = msg):
+				logging.debug("Put incoming message data to resource (CoAP): %s", str(resource))
+			else:
+				logging.warning("Failed to put incoming message data to resource (CoAP): %s", str(resource))
